5a17e7fd326d4ba82c12976db46121ceb5e11fcd
Nauana Lima <nauanalima@id.uff.br>
2018-07-15 14:38:42 -0300

    Problema 1

--- Burden_11.3/prob1a.c
+++ Burden_11.3/prob1a.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 
 #define n 3
@@ -30,6 +31,57 @@ void print(double **matrix, int row, int col){
 	}
 	printf("\n");
 }
+double **creatematrix (double **matrix, int row, int col, int *x, int h) {
+    int i;
+
+    for(i=0; i<row; i++) {	
+		if(i==0) {
+			matrix[i][i] = 2 + pow(h,2)* q(x[i+1]);
+			matrix[i][i+1] = - 1 + (h/2.)* p(x[i+1]);
+		}
+		else {
+            if(i==n-1) {
+            	matrix[i][i-1] = - 1 - (h/2.)* p(x[i+1]);
+                matrix[i][i] = 2 + pow(h, 2)* q(x[i+1]);
+            }
+            else {
+            	matrix[i][i-1] = - 1 - (h/2.)* p(x[i+1]);
+                matrix[i][i] = 2 + pow(h, 2)* q(x[i+1]);
+                matrix[i][i+1] = - 1 + (h/2.)* p(x[i+1]);
+            }
+		}
+	}
+	matrix[0][n] = -pow(h,2)*r(x[1]) + (1+(h/2.)*p(x[1]))*y(x[0]);
+    matrix[n-1][n] = -pow(h,2)*r(x[n]) + (1-(h/2.)*p(x[n]))*y(x[n+1]);
+}
+
+double jacobi(double **matrix, int row, int col, double *x1, double *x2) {
+
+	int i,j;
+	double sum, s1, s2, error;
+
+	for(i=0; i<row; i++) {	
+		sum = 0;
+		
+		for(j=0; j<col-1; j++) {	
+			if( j!= i )
+				sum += -matrix[i][j]*x1[j];
+		}
+
+		x2[i] = (sum + matrix[i][col-1])/ matrix[i][i];
+	}
+
+	for(i=0; i<row; i++) {	
+
+		s1 += pow(x2[i]-x1[i], 2);
+		s2 += pow(x2[i], 2);
+	
+	}
+	
+	error = sqrt(s1)/sqrt(s2);
+	
+	return error;
+}
 
 void exchangelines (double *line1, double *line2, int row) {
 	double temp;
@@ -42,9 +94,11 @@ void exchangelines (double *line1, double *line2, int row) {
 	}
 }
 
-double uppertriangular (double **matrix, int row, int col) {
+int uppertriangular (double **matrix, int row, int col) {
 	int i, j, k, k1, test, pivot;
 	double m, temp;
+
+	int steps = 0; 
 
 	for (k=0; k<row; k++) {
 		for (j=k; j<row; j++) {
@@ -54,6 +108,7 @@ double uppertriangular (double **matrix, int row, int col) {
 			}
 			if (test!=-1) {
 				exchangelines(matrix[k], matrix[test], row);
+				steps++;
 			}
 		}
 		for (i=k+1; i<row; i++) {
@@ -62,12 +117,13 @@ double uppertriangular (double **matrix, int row, int col) {
 				matrix[i][j] = matrix[i][j] - m*matrix[k][j];
 		}
 	}
-	return **matrix;		
+	return steps;		
 }
 
 double *reversesub (double **matrix, const int dim) {
-	int j, k, n;
-	double sum, *roots = (double*)malloc(dim*sizeof(double));
+	int j, k;
+	double sum;
+    double *roots = malloc(dim*sizeof(double));
 	roots[dim-1] = matrix[dim-1][dim]/matrix[dim-1][dim-1];
 	k = dim-2;
 	while (k>=0) {
@@ -81,6 +137,18 @@ double *reversesub (double **matrix, const int dim) {
 }
 
 int main() {
-	int i, j;
-    double h = 0.5;
+	int i, j, row = n, col = n+1;
+    double h = 0.5, x[n+2], w[n+2], x0, x1, y0, y1;
+    double **matrix;
+
+    matrix = malloc(row* sizeof(double*));
+	for( i = 0; i < col; i++ )
+        matrix[i] = malloc(col*sizeof(double));
+
+    x[0] = x0;
+
+    for(i=1; i<=n; i++)	
+		x[i] = x0 + i*h;
+    x[n+1] = x1;
+
 }
